68e56f5854592cede2bc7a22260b38132fd1b86a
ekhusainov <xluttiy@gmail.com>
2021-11-07 07:19:54 +0300

    Update data_mining.py

--- src/data_mining.py
+++ src/data_mining.py
@@ -14,7 +14,6 @@ from src.download_function.download import (
 )
 
 DEFAULT_LOGGING_PATH = "configs/logging.conf.yml"
-
 APPLICATION_NAME = "core"
 
 logger = logging.getLogger(APPLICATION_NAME)
@@ -28,15 +27,14 @@ def setup_logging():
 
 def callback_download(arguments):
     setup_logging()
-    if arguments.site_name is None or arguments.content is None:
+    if arguments.site_name is None or arguments.type_content is None:
         logger.error("Enter site name and type content.")
     else:
-        download(arguments.site_name, arguments.content,
+        download(arguments.site_name, arguments.type_content,
                  arguments.first_number, arguments.number_pages)
 
 
 def setup_parser(parser):
-    "Argparser."
     subparsers = parser.add_subparsers(
         help="choose command",
     )
@@ -49,39 +47,31 @@ def setup_parser(parser):
     download.add_argument(
         "-s", "--site",
         dest="site_name",
-        help="help defalt TODO",
+        help="Site name.",
     )
     download.add_argument(
         "-c", "--content",
-        dest="content",
-        help="help defalt TODO",
+        dest="type_content",
+        help="Type of content",
     )
     download.add_argument(
         "-f", "--first_number",
         dest="first_number",
-        help="TODO",
+        help="The number of the first post.",
         default=1,
         type=int,
     )
     download.add_argument(
         "-n", "--number_pages",
         dest="number_pages",
-        help="TODO",
+        help="Number of posts.",
         default=500,
         type=int,
-    )
-    download.add_argument(
-        "-d", "--directory",
-        dest="directory",
-        help="TODO",
-        default="output",
-        type=str,
     )
     download.set_defaults(callback=callback_download)
 
 
 def main():
-    "Our int main."
     parser = ArgumentParser(
         prog=APPLICATION_NAME,
         description="Download ochoba sites",
